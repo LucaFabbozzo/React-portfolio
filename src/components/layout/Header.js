7e8a1ffd41567c2ba9e909882dcb1022080e7cf3
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-29 08:33:00 +0200

    navbar with NavLink

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header>
-      <h1>Luca Fabbozzo Wed Developer</h1>
+      <div className='logo'>
+        <span>L</span>
+        <h3>Luca Fabbozzo Wed Developer</h3>
+      </div>
       <nav>
         <ul>
-          <li>Nav</li>
+          <li>
+            <NavLink to="/start">Start</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/curriculum">Curriculum</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
