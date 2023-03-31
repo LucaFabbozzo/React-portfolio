33f751729372207440b13a9c50a1b5ff6e52585d
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-31 13:01:48 +0200

    show images and styles cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,18 +6,23 @@ export const Portfolio = () => {
   return (
     <div className="page">
       <h1 className="heading">Portfolio</h1>
-      
-      {
-        works.map(work => {
+
+      <section className="works">
+        {works.map((work) => {
           return (
-            <article key={work.id}>
+            <article key={work.id} className="work-item">
+              <div className="mask">
+                <img src={"/images/" + work.id + ".png"} alt={work.name} />
+              </div>
               <span>{work.category}</span>
-              <h2><Link to={"/project/" + work.id}>{work.name}</Link></h2>
+              <h2>
+                <Link to={"/project/" + work.id}>{work.name}</Link>
+              </h2>
               <h3>{work.tecnology}</h3>
             </article>
           );
-        })
-      }
+        })}
+      </section>
     </div>
   );
 }
--- src/components/data/works.js
+++ src/components/data/works.js
@@ -27,7 +27,7 @@ export const works = [
       "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. ",
   },
   {
-    id: "linkedIn",
+    id: "linkedin",
     name: "LinkedIn",
     url: "linkedIn.com",
     tecnology: "HTML, CSS, JS",
